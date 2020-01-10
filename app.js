e3f2402e47463fe20ab151101365b460724f1ceb
Hsin Ling Hu <hsinkava11@gmail.com>
2020-01-09 19:39:33 -0800

    first version

--- app.js
+++ app.js
@@ -1,43 +1,103 @@
+'use strict';
 /**
  * Example store structure
  */
+
 const store = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'How is the function called in JavaScript?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'call ThinkfulFn();',
+        'call function ThinkfulFn();',
+        'ThinkfulFn();',
+        'function ThinfulFn();'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'ThinkfulFn();'
     },
     {
-      question: 'What is the current year?',
+      question: 'How to write (If “i” is NOT equal to 8) code?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'if(i<>8)',
+        'if i<8',
+        'if(i!=8)',
+        'if i!=8'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'if(i!=8)'
+    },
+    {
+      question: 'What does CSS stand for?',
+      answers: [
+        'Colorful Style Sheets',
+        'Cascading Style Sheets',
+        'Computer System Sheets',
+        'Cooperation Style System'
+      ],
+      correctAnswer: 'Cascading Style Sheets'
+    },
+    {
+      question: 'Which function of an Array object calls a function for each element in the array?',
+      answers: [
+        'map()',
+        'function()',
+        'forEveryItem()',
+        'if()'
+      ],
+      correctAnswer: 'map()'
+    },
+    {
+      question: 'Choose the correct HTML element for the largest heading:',
+      answers: [
+        '<head>',
+        '<heading>',
+        '<h1>',
+        '<ul>'
+      ],
+      correctAnswer: '<h1>'
     }
   ],
   questionNumber: 0,
   score: 0
 };
 
-/**
- *
- * Your app should include a render() function, that regenerates
- * the view each time the store is updated. See your course
- * material, consult your instructor, and reference the slides
- * for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
+function startQuiz() {
+  // The starting screen should have a button that users can click to start the quiz.
+}
+
+function preventNoAns() {
+  // Users should not be able to skip questions.
+}
+
+function displayQuesNum() {
+  // Users should also be able to see which question they're on and current score 
+  //**SPLIT FUNCTIONS FOR SCORE AND QUESTION NUMBER??**
+}
+
+function displayTotalScore() {
+  // Users should be shown their overall score at the end of the quiz (correct out of total)
+}
+
+function restartQuiz() {
+  // Users should be able to start a new quiz
+}
+
+function render() {
+  //Your app should include a render() function, that regenerates the view each time the store is updated.
+}
+
+function main() {
+
+}
+
+$(main);
+
+/*
+CHECKLIST:
+
+-Render answer choices in a <form>
+-Follow a11y best practices
+-Use responsive design
+-Be fully usable by keyboard (which will be easy enough if you start with a form)
+-Use for= attributes on labels that match the ID of the matching input.
  */
