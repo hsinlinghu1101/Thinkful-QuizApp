e776c71cf2f450077747f97577d568463e7d5bc7
hsinlinghu1101 <47201201+hsinlinghu1101@users.noreply.github.com>
2020-01-10 18:16:05 -0800

    revised version

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
  */
 
 const store = {
-  // 5 or more questions are required
+  // 5 or more questions are
   questions: [
     {
-      question: 'How is the function called in JavaScript?',
+      question: 'How is a function invoked in JavaScript?',
       answers: [
         'call ThinkfulFn();',
         'call function ThinkfulFn();',
@@ -19,7 +19,7 @@ const store = {
     {
       question: 'How to write (If “i” is NOT equal to 8) code?',
       answers: [
-        'if(i<>8)',
+        'if(i ! 8)',
         'if i<8',
         'if(i!=8)',
         'if i!=8'
@@ -49,45 +49,154 @@ const store = {
     {
       question: 'Choose the correct HTML element for the largest heading:',
       answers: [
-        '<head>',
-        '<heading>',
-        '<h1>',
-        '<ul>'
+        'head',
+        'heading',
+        'h1',
+        'ul'
       ],
-      correctAnswer: '<h1>'
+      correctAnswer: 'h1'
     }
   ],
+  quizStart:false,
   questionNumber: 0,
-  score: 0
+  score: 0,
+  answerCheck: undefined
 };
 
-function startQuiz() {
-  // The starting screen should have a button that users can click to start the quiz.
+// ****TEMPLATE FUNCTIONS****
+
+function generateStartPage() {
+  return `
+  <form class="startPage">
+  <img src="https://img.icons8.com/color/48/000000/light.png"><p class="light">The quiz contains 5 questions and  no time limit. </p></br>
+  <img src="https://img.icons8.com/color/48/000000/light.png"><p class="light">Let's see how much you know, or don't know, about Javascript, CSS and HTML.</p></br>
+  <p id="start">Good Luck !</p>
+  <button type='submit' class='start-button'>Start Quiz</button> 
+  
+  </form>
+  `;
 }
 
-function preventNoAns() {
-  // Users should not be able to skip questions.
+function generateCurrentQuestion() {
+  return `
+    <form class='current-question-choices'>
+    <p class="quesline">Question <span class='question-number'>${store.questionNumber}</span> of 5</p>
+    <p class='current-question-text'>${store.questions[store.questionNumber-1].question}</p>
+    <ul style='list-style-type: none;'>
+      <li><label for ='a' class='choice-a'>A.<input type='radio' name='choice' value=${store.questions[store.questionNumber-1].answers[0]} id = 'a' tabindex='0' required>${store.questions[store.questionNumber-1].answers[0]}</label></li>
+      <li><label for ='b' class='choice-b'>B.<input type='radio' name='choice' value=${store.questions[store.questionNumber-1].answers[1]} id = 'b'>${store.questions[store.questionNumber-1].answers[1]}</label></li>
+      <li><label for ='c' class='choice-c'>C.<input type='radio' name='choice' value=${store.questions[store.questionNumber-1].answers[2]} id = 'c'>${store.questions[store.questionNumber-1].answers[2]}</label></li>
+      <li><label for ='d' class='choice-d'>D.<input type='radio' name='choice' value=${store.questions[store.questionNumber-1].answers[3]} id = 'd'>${store.questions[store.questionNumber-1].answers[3]}</label></li>
+    </ul>
+    <button type='button' id='check-answer'>Check Answer</button>
+    <button type="submit" class='restart-button'>Restart</button> 
+    </form>
+  `;
 }
 
-function displayQuesNum() {
-  // Users should also be able to see which question they're on and current score 
-  //**SPLIT FUNCTIONS FOR SCORE AND QUESTION NUMBER??**
+function generateCorrectPage() {
+  return `
+  <form class='correct-answer-form'>
+  <h2>Correct Answer!</h2>
+  <p>Correct Answer: ${store.questions[store.questionNumber-1].correctAnswer}</p>
+  <button type='button' class='next-question'>Next Question</button>
+  <p class="tellscore">Current Score: <span class='current-correct'>${store.score}</span> of ${store.questionNumber}</p>
+  </form>
+  `;
 }
 
-function displayTotalScore() {
-  // Users should be shown their overall score at the end of the quiz (correct out of total)
+function generateWrongPage() {
+  return `
+  <form class='wrong-answer-form'>
+  <h2>Wrong Answer!</h2>
+  <p>Correct Answer: ${store.questions[store.questionNumber-1].correctAnswer}</p>
+  <button type='button' class='next-question'>Next Question</button>
+  <p class="tellscore">Current Score: <span class='current-correct'>${store.score}</span> of ${store.questionNumber}</p>
+  </form>
+  `;
 }
 
-function restartQuiz() {
-  // Users should be able to start a new quiz
+function generateQuizComplete() {
+  return `
+  <form class = "result">
+  <label for="check result">Completed the Quiz</label>
+  <p id="start">High Five!<img src="https://img.icons8.com/cute-clipart/64/000000/high-five.png"></p>
+  <p class="quesline">You got <span class ="current-correct">${store.score}</span> out of 5!</p>
+  <button type="submit" class='restart-button'>Restart</button>
+</form>`;
 }
+
+// **** RENDER FUNCTION ****
 
 function render() {
-  //Your app should include a render() function, that regenerates the view each time the store is updated.
+  if (store.questionNumber === 0) {
+    $('main').html(generateStartPage());
+  } else if (store.questionNumber > 0 && store.questionNumber <=5) {
+    if (store.answerCheck === true) {
+      $('main').html(generateCorrectPage());
+    } else if (store.answerCheck === false) {
+      $('main').html(generateWrongPage());
+    } else {
+      $('main').html(generateCurrentQuestion());
+    }
+  } else {
+    $('main').html(generateQuizComplete());
+  }
+  handleStartQuiz();
+  handleCheckAnswer();
+  handleRestartQuiz();
+  handleNextQuestion();
+}
+
+// **** HANDLE FUNCTIONS ****
+
+function handleStartQuiz() {
+  // The starting screen should have a button that users can click to start the quiz.
+  $('.start-button').on('click', function(e){
+    e.preventDefault();
+    store.questionNumber = 1;
+    render();
+  });
+}
+
+function handleCheckAnswer(){
+  $('#check-answer').on('click', function(e){
+    e.preventDefault();
+    console.log('hello'); //Delete when done
+    let selectAnswer = $('input[type = "radio"]:checked').attr('value');
+    console.log(selectAnswer);
+    console.log(typeof selectAnswer);
+    console.log(store.questions[store.questionNumber-1].correctAnswer);
+    console.log(typeof store.questions[store.questionNumber-1].correctAnswer);
+    if (selectAnswer === store.questions[store.questionNumber-1].correctAnswer){
+      store.score ++;
+      store.answerCheck = true;
+    }else{
+      store.answerCheck = false;
+    }
+    render();
+  });
+}
+
+function handleNextQuestion() {
+  $('.next-question').on('click', function() {
+    store.answerCheck = undefined;
+    store.questionNumber++;
+    render();
+  });
+}
+
+function handleRestartQuiz() {
+  // Users should be able to start a new quiz
+   $('.restart-button').on('click', function() {
+    store.questionNumber = 0;
+    render();
+   });
 }
 
 function main() {
-
+  render();
+  handleStartQuiz();
 }
 
 $(main);
@@ -95,9 +204,6 @@ $(main);
 /*
 CHECKLIST:
 
--Render answer choices in a <form>
--Follow a11y best practices
+-Prevent checking answer if no choice is made
 -Use responsive design
--Be fully usable by keyboard (which will be easy enough if you start with a form)
--Use for= attributes on labels that match the ID of the matching input.
  */
